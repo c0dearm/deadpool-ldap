883d3d07401d09bc51a6e1334de64ae7d6926a4b
Aitor Ruano <codearm@pm.me>
2020-11-13 12:51:16 +0100

    add support for actix runtime via feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod errors;
 
 use async_trait::async_trait;
-use ldap3::{drive, exop::WhoAmI, Ldap, LdapConnAsync, LdapError as Error};
+use ldap3::{exop::WhoAmI, Ldap, LdapConnAsync, LdapError as Error};
 
 pub type Pool = deadpool::managed::Pool<Ldap, errors::LdapError>;
 pub struct Manager(pub &'static str);
@@ -10,7 +10,14 @@ pub struct Manager(pub &'static str);
 impl deadpool::managed::Manager<Ldap, Error> for Manager {
     async fn create(&self) -> Result<Ldap, Error> {
         let (conn, ldap) = LdapConnAsync::new(self.0).await?;
-        drive!(conn);
+        #[cfg(feature = "default")]
+        ldap3::drive!(conn);
+        #[cfg(feature = "rt-actix")]
+        actix_rt::spawn(async move {
+            if let Err(e) = conn.drive().await {
+                log::warn!("LDAP connection error: {}", e);
+            }
+        });
         Ok(ldap)
     }
     async fn recycle(&self, conn: &mut Ldap) -> deadpool::managed::RecycleResult<Error> {
